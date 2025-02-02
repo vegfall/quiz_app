84ccddee5be2855c2a13cdd42f78272cc64ec955
vegfall <vegfall@gmail.com>
2025-02-02 08:38:59 +0100

    Added routes, and simple home page

--- frontend/src/pages/application/app.tsx
+++ frontend/src/pages/application/app.tsx
@@ -1,5 +1,13 @@
 import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "../home/homePage";
 
 export default function App() {
-  return <h1>Hello World! :)</h1>;
+  return (
+    <Router>
+      <Routes>
+        <Route path={"/"} element={<HomePage />} />
+      </Routes>
+    </Router>
+  );
 }
