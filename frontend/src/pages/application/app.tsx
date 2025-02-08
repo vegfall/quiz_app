93d4647ef6718852ebf6b254dd0246abba2a7a06
vegfall <vegfall@gmail.com>
2025-02-08 19:00:41 +0100

    Added css styling to frontend

--- frontend/src/pages/application/app.tsx
+++ frontend/src/pages/application/app.tsx
@@ -1,13 +1,18 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import HomePage from "../home/homePage";
+import "../application/app.css";
 
 export default function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path={"/"} element={<HomePage />} />
-      </Routes>
-    </Router>
+    <div className="main_container">
+      <div className="main_block">
+        <Router>
+          <Routes>
+            <Route path={"/"} element={<HomePage />} />
+          </Routes>
+        </Router>
+      </div>
+    </div>
   );
 }
--- frontend/src/pages/home/homePage.tsx
+++ frontend/src/pages/home/homePage.tsx
@@ -1,5 +1,23 @@
 import React from "react";
 
 export default function HomePage() {
-  return <h1>Hello from Home :)</h1>;
+  return (
+    <div>
+      <h1 className="title">Welcome to my QuizApp</h1>
+      <hr />
+      <h3>
+        Please select if you wish to start a new Quiz, or if you want to play an
+        existing one shown below...
+      </h3>
+      <hr />
+      <h3>Username:</h3>
+      <input type="text" />
+      <button>Start New Quiz</button>
+      <hr />
+      <h3>Session Key:</h3>
+      <input type="text" />
+      <button>Join Existing Quiz</button>
+      <h2>Available Sessions</h2>
+    </div>
+  );
 }
